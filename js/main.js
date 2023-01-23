1d081e5d7c6a9665533dcc76b7424fb9ccac675c
mrFTO <ntoon@ya.ru>
2023-01-24 01:01:10 +0300

    Update day 11

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 20,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+});
